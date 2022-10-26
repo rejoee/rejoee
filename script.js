9bebb81aac611f09d2d13a26cea3b1034df4dada
rejoee <112939109+rejoee@users.noreply.github.com>
2022-10-26 18:23:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+get = id => document.getElementById(id)
+sleep = delay => new Promise((resolve) => setTimeout(resolve, delay))
+async function started() {
+    for (char of "Regi Weboldala".split("")) {
+        get("h1").innerHTML += char;
+        await sleep(100);
+    }
+}
+function orarend() {
+    get("fooldal").style.display = "none";
+    get("orarend").style.display = "unset";
+}
+function fooldal() {
+    get("orarend").style.display = "none";
+    get("fooldal").style.display = "unset";
+}
